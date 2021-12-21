04f9235a21d7799689136e9cf8b5271f07f8c37f
Ravikiran <ravi.panchaneni@gmail.com>
2021-12-21 17:48:38 +0530

    initial entires added

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -6,7 +6,7 @@ import App from './App';
 // Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   const history = defaultHistory || createMemoryHistory({
-    initialPath: [initialPath]
+    initialEntries: [initialPath]
   });
   if(onNavigate){
     history.listen(onNavigate);
